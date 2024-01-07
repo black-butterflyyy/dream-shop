93c4b02322ad59aa40a68b5e84f0c5b9e3b8fe49
Hosna <blackbutterfllly@gmail.com>
2024-01-07 13:27:45 +0330

    Update the path of cart item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,7 @@
 ﻿import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
-import CartItem from '../../cart-item/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = () => {
