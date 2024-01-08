87d47f0b3dc97b0fc4541d30a2a702588a3434fa
Hosna <blackbutterfllly@gmail.com>
2024-01-08 23:13:15 +0330

    Add nested routes inside shop route Add categories preview and category preview route components inside shop component

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,30 @@
+﻿import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import { CategoriesContext } from '../../contexts/categories.context';
+import './category.styles.scss';
+import ProductCard from '../../components/product-card/product-card.component';
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className='category-route-container'>
+      <h2 className='title'>{category}</h2>
+      <div className='category-route-products'>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
