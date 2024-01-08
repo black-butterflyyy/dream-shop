40d96856e855be75554424cc11261b631807b643
Hosna <blackbutterfllly@gmail.com>
2024-01-08 23:10:33 +0330

    Update products context Rename products context to categories context, add new values to categories context

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,26 @@
+﻿import { useState, useEffect, createContext } from 'react';
+import { getCategoriesAndDocument } from '../utils/firebase/firebase.util';
+
+export const CategoriesContext = createContext({
+  categoriesMap: [],
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocument();
+      setCategoriesMap(categoriesMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
